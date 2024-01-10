53fb481baf7bf4db2c8688ca58a0c0b36d02796d
hantran6 <s3963227@rmit.edu.vn>
2024-01-10 22:44:43 +0700

    Han's config site update

--- src/lambdas/server/config-site.js
+++ src/lambdas/server/config-site.js
@@ -23,23 +23,74 @@ async function getSiteCredential(email) {
   return { site, password };
 }
 
+
 export const configSite = async (req, res) => {
-  const siteCredential = await getSiteCredential(req.userEmail);
-  const siteName = siteCredential.site;
-  const sitePassword = siteCredential.password;
+  const { email, siteName, sitePassword } = req.body;
 
-  console.log('siteName', siteName);
-
-  const isCreateRouteSuccess = serverDomain.createSubDomain(siteName);
-  if (!isCreateRouteSuccess) {
-    res.status(400).json({ message: 'Site name must be unique' });
+  // Check if all fields are provided
+  if (!email || !siteName || !sitePassword) {
+    return res.status(400).json({ message: 'Missing required fields' });
   }
 
-  res.status(200).json({
-    message: 'Config site successfully',
-    siteName: siteName,
-    sitePassword: sitePassword,
-    siteDomain: `${siteName}.vertex-erp.com`,
-  });
+  // Create record and check if site exists
+  const isCreateRouteSuccess = serverDomain.createSubDomain(siteName);
+  if (!isCreateRouteSuccess) {
+    return res.status(400).json({ message: 'Site name must be unique' });
+  }
 
+  // Save user details
+  const { dynamoDB } = databases;
+  const tableName = dynamoDB.getTable('users');
+
+  if (!tableName) {
+    return res.status(500).json({ error: 'Table name not found' });
+  }
+
+  const params = {
+    TableName: 'users',
+    IndexName: 'email-index',
+    KeyConditionExpression: 'email = :email',
+    ExpressionAttributeValues: {
+      ':email': email
+    },
+  };
+
+  try{
+    const data = await dynamoDB.queryItems(params);
+    console.log('data', data);
+
+    const items = data.Items;
+    if (!items || items.length === 0) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    const batchUpdateParams = items.map((item) => {
+      return {
+        PutRequest: {
+          Item: {
+            userTier: item.userTier,
+            email: item.email,
+            dateExpired: item.dateExpired,
+            isExpired: item.isExpired,
+            site: siteName,
+            sitePassword: sitePassword,
+          },
+        },
+      };
+    });
+    console.log('batchUpdateParams', batchUpdateParams);
+    const newData = await dynamoDB.batchUpdate(batchUpdateParams, tableName);
+    console.log('newData', newData);
+
+    res.status(200).json({
+      message: 'Config site successfully',
+      siteName: siteName,
+      sitePassword: sitePassword,
+      siteDomain: `${siteName}.vertex-erp.com`,
+    });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }  
 };
+
--- src/lambdas/server/index.js
+++ src/lambdas/server/index.js
@@ -3,6 +3,6 @@ import { authMiddleware } from '../../middlewares/auth.js';
 import { configSite } from './config-site.js';
 
 const router = express.Router();
-router.get('/config-site', authMiddleware, configSite);
+router.post('/config-site', authMiddleware, configSite);
 
 export const serverRouter = router;
